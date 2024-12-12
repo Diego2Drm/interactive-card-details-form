865aea21784ff6644edef93b8bec917a6bef5d6d
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-12-12 13:27:24 -0600

    Project Initialization and Tailwind Configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-Very-dark-violet text-center font-bold font-SpaceGrotesk">Hello React js + TypeScript + Tailwind Css</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        //Primary
+        'Red-input-errors': '#ff5252',
+        // Neural
+        'Light-grayish-violet': '#dedddf',
+        'Dark-grayish-violet': '#8e8593',
+        'Very-dark-violet': '#21092f',
+      },
+      fontFamily: {
+        'SpaceGrotesk': ["Space Grotesk", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
