6060bc98342fb8ed9daad18c914d01abf437f1f0
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-12-18 12:29:39 -0600

    Input validation

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ export interface CardFormData {
   expYear: string;
   cvc: string;
 }
+export interface CardFormDataErrors {
+  cardName?: string;
+  cardNumber?: string;
+  expMonth?: string;
+  expYear?: string;
+  cvc?: string;
+}
 
 
 function App() {
@@ -21,11 +28,17 @@ function App() {
     expYear: '',
     cvc: '',
   });
+
   const [showData, setShowData] = useState<boolean>(false);
   const [submitData, setSubmitData] = useState<CardFormData | null>(null);
+  const [errors, setErrors] = useState<CardFormDataErrors>({})
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
+    
+    if( (name === 'cvc' || name === 'expYear' ||  name === 'expMonth' || name === 'cardNumber') && !/^\d*$/.test(value)){
+      return;
+    }
     setFormData({
       ...formData, 
       [name]: value
@@ -34,6 +47,30 @@ function App() {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const newErrors : CardFormDataErrors= {};
+    if(!formData.cardName || !/^[a-zA-Z\s]*$/.test(formData.cardName)){
+      newErrors.cardName = "Can't be blank and must contain only letters";
+    }
+    if(!formData.cardNumber || formData.cardNumber.length !== 16 ){
+      newErrors.cardNumber = "Can't be blank and must be 16 digits"
+    }
+    if(!formData.expMonth || formData.expMonth.length !== 2 ){
+      newErrors.expMonth = "Can't be blank and must be 2 digits"
+    }
+    if(!formData.expYear || formData.expYear.length !== 2 ){
+      newErrors.expYear = "Can't be blank and must be 2 digits"
+    }
+    if(!formData.cvc || formData.cvc.length !== 3){
+      newErrors.cvc = "Can't be blank and must be 3 digits"
+    }
+ 
+
+    if(Object.keys(newErrors).length > 0){
+      setErrors(newErrors);
+      return;
+    }
+
     setSubmitData(formData);
     setFormData({
       cardName: '',
@@ -42,14 +79,15 @@ function App() {
       expYear: '',
       cvc: '',
     })
-    setShowData(true)
+    setShowData(true);
+    setErrors({});
     console.log(formData);
   }
 
   return (
     <section className="lg:flex">
       <Cards formData={submitData} showData={showData} />
-      <Form formData={formData} handleSubmit={handleSubmit} handleChange={handleChange} />
+      <Form formData={formData} handleSubmit={handleSubmit} handleChange={handleChange} errors={errors}/>
       {/* <Exapmle /> */}
     </section>
   )
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -13,15 +13,20 @@ const Cards: React.FC<CardsProps> = ({ formData, showData }) => {
     <section className='bg-bgMianMobile h-56 relative lg:bg-bgMianDesktop bg-cover lg:h-screen lg:w-1/2'>
       <figure className='w-72 h-40 absolute right-8 top-5 bg-cardback bg-cover rounded-lg flex justify-end pt-[4.4rem] pr-10 cardBack-desktop'>
         <p className='text-white text-xs lg:text-lg'>
-        {showData && formData && <span>{formData.cvc}</span>}
+          {/* {showData && formData && <span>{formData.cvc}</span>} */}
+          {showData && formData ? formData.cvc : '00'}
         </p>
       </figure>
       <figure className='absolute -bottom-11 left-5 w-72 h-40 bg-cardFront bg-cover rounded-lg p-5 cardFront-desktop'>
         <img src={cardLogo} alt="card-logo" className='w-14 lg:w-24' />
         <div className='mt-7'>
-          <p className='text-white text-lg tracking-widest lg:text-xl'>{showData && formData ? formData.cardNumber : '0000 0000 0000 0000'}</p>
+          <p className='text-white text-lg tracking-widest lg:text-xl'>
+            {showData && formData ? formData.cardNumber.match(/.{1,4}/g)?.join(' ') : '0000 0000 0000 0000'}
+          </p>
           <div className='flex justify-between mt-4 text-white text-sm lg:text-lg'>
-            <p className='uppercase'>{showData && formData ? formData.cardName : 'Jane Appleseed'}</p>
+            <p className='uppercase'>
+              {showData && formData ? formData.cardName : 'Jane Appleseed'}
+            </p>
             <p>{showData && formData ? `${formData.expMonth} / ${formData.expYear}` : '00 / 00'}</p>
           </div>
         </div>
