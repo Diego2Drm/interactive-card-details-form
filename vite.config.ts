d7ff008a4156236e84ee9db10b4eba9e40fc86b0
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-12-18 21:57:09 -0600

    Change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/interactive-card-details-form",
+  base: "https://Diego2Drm.github.io/interactive-card-details-form",
   plugins: [react()],
 })
